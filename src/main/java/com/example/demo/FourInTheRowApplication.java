8aa89bd0e355d772fd52427c8346c57c2af5398f
Robertino Solanas <r.sol@gmx.de>
2025-03-14 20:33:13 +0100

    spring

---
+++ src/main/java/com/example/demo/FourInTheRowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourInTheRowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourInTheRowApplication.class, args);
+	}
+
+}
